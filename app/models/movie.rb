50881441f6e3d806cfb88b87847d0e01032196d6
irvingvillasenor <irving@pipersolution.com>
2020-06-16 09:38:38 -0500

    adds associations and join table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
   validates :director, presence: true, length: { minimum: 2 }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
